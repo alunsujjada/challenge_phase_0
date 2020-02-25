6a882732e979e0787b5ac35bd94affca6421e2b4
alunsujjada <48158274+alunsujjada@users.noreply.github.com>
2020-02-25 13:48:35 +0700

    Add files via upload

---
+++ looping/ganjil-genap.js
@@ -0,0 +1,29 @@
+for(let index=1;index<=100;index++){
+    if(index % 2==0){
+        console.log('GENAP');
+    }
+    else{
+        console.log('GANJIL');
+    }
+}
+
+for (index2=1;index2<=100;index2+=2){
+    if(index2 % 3==0){
+        console.log(`${index2} kelipatan 3`);
+    }
+    
+}
+
+for (index5=1;index5<=100;index5+=5){
+    if(index5 % 6==0){
+        console.log(`${index5} kelipatan 6`);
+    }
+    
+}
+
+for (index9=1;index9<=100;index9+=9){
+    if(index9 % 10==0){
+        console.log(`${index9} kelipatan 10`);
+    }
+    
+}
---
+++ looping/looping-for.js
@@ -0,0 +1,11 @@
+console.log('LOOPING PERTAMA');
+
+for (let index=1;index <= 20;index++){
+    console.log(`${index} - I love coding`);
+}
+
+console.log('LOOPING KEDUA');
+
+for(let index=20;index > 0;index--){
+    console.log(`${index} - I will become fullstack developer`);
+}
