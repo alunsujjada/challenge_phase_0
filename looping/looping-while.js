7129419a1edd790516895bca3c2844cbdaa5ca58
alunsujjada <48158274+alunsujjada@users.noreply.github.com>
2020-02-25 13:47:46 +0700

    Create looping-while.js

---
+++ looping/looping-while.js
@@ -0,0 +1,13 @@
+
+var number = 0;
+console.log('LOOPING PERTAMA');
+while(number<20){
+    number += 2;
+    console.log(`${number} - I love coding`);
+}
+
+console.log('LOOPING KEDUA');
+while(number > 0){
+    console.log(`${number} - I love coding`);
+    number -= 2;
+}
