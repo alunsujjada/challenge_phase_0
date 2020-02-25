48e9fdffdbd7916c63cc964259774629d9109cc2
alunsujjada <48158274+alunsujjada@users.noreply.github.com>
2020-02-25 11:35:36 +0700

    Add files via upload

---
+++ tanggalformat.js
@@ -0,0 +1,46 @@
+var tanggal = 21;
+var bulan=  8; 
+var tahun=  2019; 
+var bulanInString;
+
+switch (bulan){
+    case 1 :
+        bulanInString ='Januari'
+        break
+    case 2 :
+        bulanInString ='Februari'
+        break
+    case 3 :
+        bulanInString ='Maret'
+        break
+    case 4 :
+        bulanInString ='April'
+        break
+    case 5 :
+        bulanInString ='Mei'
+        break
+    case 6 :
+        bulanInString ='Juni'
+        break
+    case 7 :
+        bulanInString ='Juli'
+        break
+    case 8 :
+        bulanInString ='Agustus'
+        break
+    case 9 :
+        bulanInString ='September'
+        break
+    case 10 :
+        bulanInString ='Oktober'
+        break
+    case 11 :
+        bulanInString ='November'
+        break
+    case 12 :
+        bulanInString ='Desember'
+        break
+        
+}
+
+console.log(`${tanggal} ${bulanInString} ${tahun}`)
