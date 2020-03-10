5d3ce0d568cda31f4758e56bfe275a22eeec1d08
alunsujjada <48158274+alunsujjada@users.noreply.github.com>
2020-03-10 09:40:10 +0700

    Add files via upload

--- totalDigit.js
+++ totalDigit.js
@@ -1,23 +1,20 @@
 function totalDigitRekursif(angka) {
   // you can only write your code here!
   var total = 0
-  var stringNum = String(angka)
-  if(stringNum == '0'){
+  if(angka == 0){
       return total
   }
   else{
-      var getNum = stringNum.split('')
-      total += parseInt(getNum[0])
-      getNum.shift()
-      angka = getNum.join('')
-      var nextNum = Number(angka)
-      return total + totalDigitRekursif(nextNum)
+    var sisa = angka % 10
+    var bagi = Math.floor(angka/10)
+    total += sisa
+    return total + totalDigitRekursif(bagi)
   }
 }
 
 // TEST CASES
-console.log(totalDigitRekursif(512)); // 8
-console.log(totalDigitRekursif(1542)); // 12
+console.log(totalDigitRekursif(51230)); // 8
+console.log(totalDigitRekursif(15429)); // 12
 console.log(totalDigitRekursif(5)); // 5
 console.log(totalDigitRekursif(21)); // 3
 console.log(totalDigitRekursif(11111)); // 5
