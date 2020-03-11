edc9f44ac1e032a01f31caf1ea9903626882476c
alunsujjada <48158274+alunsujjada@users.noreply.github.com>
2020-03-11 09:27:16 +0700

    Add files via upload

--- meleeRange.js
+++ meleeRange.js
@@ -2,33 +2,58 @@ function meleeRangedGrouping (str) {
     var ranged = []
     var melee = []
     var data=[]
+    var words =''
+    var groups =''
+    var isWord = false
+
     if(str.length==0){
         return data
     }
     
-    var strSplit = str.split(',')
-    for(var i=0;i<strSplit.length;i++){
-        var dashSplit = strSplit[i].split('-')
-        if(dashSplit[1]=='Ranged'){
-            ranged.push(dashSplit[0])
+    for(var i=0;i<str.length;i++){
+        if(str[i] !='-' && str[i] !=',' && isWord == false){
+            words += str[i]
         }
-        else{
-            melee.push(dashSplit[0])
+        else if(str[i]=='-'){
+            isWord = true   
+        }
+
+        if(isWord==true && str[i]!='-'){
+            groups += str[i]
+            if(groups=='Ranged'){
+                ranged.push(words)
+                groups=''
+                words =''
+                isWord=false
+                
+
+            }
+            else if(groups=='Melee'){
+                melee.push(words)
+                groups=''
+                words =''
+                isWord = false
+            }
         }
     }
+
     data.push(ranged)
     data.push(melee)
-    
-  //your code here
-  return data
+
+    return data
 }
 
 // TEST CASE
 
-console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
-// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+ console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
 
-console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
-// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+ console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// // [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
 
-console.log(meleeRangedGrouping('')); // []
+ console.log(meleeRangedGrouping('')); // []
+
+
+    
+    
+ 
